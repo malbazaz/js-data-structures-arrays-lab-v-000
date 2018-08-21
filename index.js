0cbb133cc477621746b03bd4ec4aa8b3aeabe80a
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-21 17:01:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ const newdrivers =[name, ...drivers]
 return newdrivers;
 }
 function removeLastDriver(name){
-const newdrivers = drivers.slice(0, drivers.length-2);
+const newdrivers = drivers.slice(0, drivers.length-1);
 return newdrivers;
 }
